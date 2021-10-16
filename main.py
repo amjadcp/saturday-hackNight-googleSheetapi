da66b891214b424c80813a865334a9f49d45aad9
Amjad C P <84246684+amjadcp@users.noreply.github.com>
2021-10-16 13:34:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("Saturdat Hack Night Sheet").sheet1 
+
+data = sheet.get_all_records() 
+
+numRows = sheet.row_count 
+pprint(data)
